4f6fb797534e2f3f5e65cfcedc8b24ceeb61dc28
grabowCommuter <grabow.commuter@gmail.com>
2019-01-15 21:40:07 +0100

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,63 +1,18 @@
 {
-    "moduleId" : "grabowCommuter/googleSatTraffic",
-    "title" : "Satellite + Traffic",
-    "subtitle" : "www.google.com",
-    
-    "backButton" : false,
-    "enableGPS" : true,
-    "zoomControl" : false,
-    "screenLockRot" : false,
-    "reload" : true,
-    
-    "shouldOverrideUrlLoading1" : "http",
-    "shouldOverrideUrlLoading2" : "https",
+  "moduleId" : "grabowCommuter/googleSatTraffic",
+  "title" : "Satellite + Traffic",
+  "subtitle" : "Satellite View",
 
-    "loadUrl" : null,
+  "backButton" : true,
+  "enableGPS" : true,
+  "zoomControl" : false,
+  "screenLockRot" : false,
+  "reload" : true,
 
-    "loadDataWithBaseUrl1" : null,
-    "loadDataWithBaseUrl3" : "text/html",
-    "loadDataWithBaseUrl4" : "utf-8",
-    "loadDataWithBaseUrl5" : null,
-					
-    "browserLaunchLink" : "http://www.google.com",
-    
-    "loadDataWithBaseUrl2" : "
-    <!DOCTYPE html>
-<html>
-  <head>
-    <meta name='viewport' content='initial-scale=1.0, user-scalable=no'>
-    <meta charset='utf-8'>
-    <title>Traffic layer</title>
-    <style>
-      html, body, #map-canvas {
-        height: 100%;
-        margin: 0px;
-        padding: 0px
-      }
-    </style>
-    <script src='https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true'></script>
-    <script>
-function initialize() {
-  var myLatlng = new google.maps.LatLng(#lat#, #lng#);
-  var mapOptions = {
-    zoom: #zoom#,
-    center: myLatlng,
-    mapTypeId: google.maps.MapTypeId.HYBRID
-  }
+  "shouldOverrideUrlLoading1" : "http",
+  "shouldOverrideUrlLoading2" : "https",
 
-  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  "loadUrl" : "https://web-commuter.firebaseapp.com/satmap.html?lat=#lat#&lng=#lng#&zoom=#zoom#&traffic=on",
 
-  var trafficLayer = new google.maps.TrafficLayer();
-  trafficLayer.setMap(map);
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
-
-    </script>
-  </head>
-  <body>
-    <div id='map-canvas'></div>
-  </body>
-</html>
-    "
+  "browserLaunchLink" : "https://web-commuter.firebaseapp.com/satmap.html"
 }
